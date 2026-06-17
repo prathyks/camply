5fb62f39098be42a51987e03c130fd312dfeca4d
Prateek Shankar <prathyks@gmail.com>
2026-06-17 14:03:59 -0700

    fix: Only lower minimum polling interval to 3 min, keep default at 10

--- camply/config/search_config.py
+++ camply/config/search_config.py
@@ -13,7 +13,7 @@ class SearchConfig:
     """
 
     POLLING_INTERVAL_MINIMUM: int = 3  # 3 MINUTES
-    RECOMMENDED_POLLING_INTERVAL: int = 3  # 3 MINUTES
+    RECOMMENDED_POLLING_INTERVAL: int = 10  # 10 MINUTES
     ERROR_MESSAGE: str = "No search days configured. Exiting"
     MINIMUM_CAMPSITES_FIRST_NOTIFY: int = 5
     MAXIMUM_NOTIFICATION_BATCH_SIZE: int = 20
